cc19e3be9285e7b8dcc04e138b682e32d956b212
SmileChen518 <262000730@qq.com>
2019-07-16 22:41:42 +0800

    update

--- assets/Scripts/Level.js
+++ assets/Scripts/Level.js
@@ -1,11 +1,16 @@
+var SPEED = 100;
+
 cc.Class({
     extends: cc.Component,
 
     properties: {
         ndBtn: cc.Node,
         ndResult: cc.Node,
+        ndPlayer: cc.Node,
+        ndMap: cc.Node,
         _audioTask: null,
         _audioID: null,
+        _gameStatus: 0, // 0准备，1开始，2结束
     },
 
     // LIFE-CYCLE CALLBACKS:
@@ -19,7 +24,20 @@ cc.Class({
         this.initShow();
     },
 
-    // update (dt) {},
+    update (dt) {
+        if (this._gameStatus == 1){
+            var dx = this.speed * dt;
+            this.ndPlayer.x += dx;
+            var dy = SPEED/2 * dt;
+            this.ndPlayer.y += dy;
+            this.ndMap.x -= 2*dx;
+            this.ndMap.y -= 2*dy;
+            // var pos = this._getTilePos(cc.v2(this.ndPlayer.x, this.ndPlayer.y));
+            // var id = this._layerFloor.getTileGIDAt(pos);
+            // cc.log(pos.x, pos.y, id);
+            // if (id == 0) this.gameOver();
+        }
+    },
 
     initCanvas(){
         var canvas = this.node.getComponent(cc.Canvas);
@@ -36,15 +54,21 @@ cc.Class({
     },
 
     initParas(){
-        
+        this._tiledMap = this.ndMap.getComponent('cc.TiledMap');
     },
 
     initEvent(){
         this.ndBtn.on("click", function (argument) {
-            this.gameStart();
+            if (this._gameStatus == 0)
+                this.gameStart();
+            else if (this._gameStatus == 1){
+                this.speed = -this.speed;
+                this.ndPlayer.scaleX = -this.ndPlayer.scaleX;
+            }
         }, this);
         cc.find("test", this.node).on("click", function (argument) {
-            this.ndResult.active = true;
+            this.gameOver();
+            // this.ndPlayer.setPosition(cc.v2(8.5, -210));
         }, this);
         cc.find("back", this.ndResult).on("click", function (argument) {
             this.stopAudio();
@@ -64,9 +88,25 @@ cc.Class({
 
     gameStart(){
         this.playSound("click");
-        this.ndBtn.active = false;
+        this.ndMap.active = true;
+        this._layerFloor = this._tiledMap.getLayer("floor");
+        var anim = this.ndPlayer.getComponent(cc.Animation);
+        anim.play();
+        this._gameStatus = 1;
+        // var para = Math.random() >= 0.5 ? 1 : -1;
+        var para = 1;
+        if (para > 0) this.ndPlayer.scaleX = -this.ndPlayer.scaleX;
+        this.speed = para*SPEED;
         cc.find("test", this.node).active = true;
         this.playAudio();
+    },
+
+    gameOver(){
+        this._gameStatus = 2;
+        // this.ndResult.active = true;
+        // this.ndMap.active = false;
+        var anim = this.ndPlayer.getComponent(cc.Animation);
+        anim.stop();
     },
 
     playAudio () {
@@ -82,5 +122,19 @@ cc.Class({
     playSound(sName){
         var url = cc.url.raw("resources/audio/"+sName+".mp3");
         cc.audioEngine.play(url, false);
+    },
+
+    _getTilePos(posInPixel) {
+        var mapSize = this.ndMap.getContentSize();
+        var tileSize = this._tiledMap.getTileSize();
+        var x = Math.floor(posInPixel.x / tileSize.width);
+        var y = Math.floor((mapSize.height - posInPixel.y) / tileSize.height);
+        var multi = tileSize.width/tileSize.height;
+
+        // Math.abs(posInPixel.x*tileSize.height/2)+Math.abs(posInPixel.y*tileSize.width/2)<tileSize.height*tileSize.width/4;
+        var m = (posInPixel.x+multi*(mapSize.height-posInPixel.y)-mapSize.width/2-multi*tileSize.height/2)/(multi*tileSize.height);
+        var n = (multi*(mapSize.height-posInPixel.y)-posInPixel.x+mapSize.width/2-multi*tileSize.height/2)/(multi*tileSize.height);
+        cc.log(mapSize.height, mapSize.width, tileSize.height, tileSize.width, x, y, posInPixel.x, posInPixel.y, m, n);
+        return cc.v2(x, y);
     },
 });
